7556b5d8e0df75c6dd27c58a906d3feb0aea90c2
E-jiwoo <livei07@naver.com>
2023-09-28 19:17:32 +0900

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import { useState } from "react";
+import TodoBoard from "./components/TodoBoard";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const addItem = () => {
+    console.log("I'm herererere!", inputValue);
+    setTodoList([...todoList, inputValue]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>hello world</p>
-      </header>
+    <div>
+      <input
+        type="text"
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={addItem}>추가</button>
+
+      <TodoBoard todoList={todoList} />
     </div>
   );
 }
